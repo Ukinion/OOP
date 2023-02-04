0393cbcb1a6adb5b960d6b1d568cbd18f9673a7e
Ukinion <114142826+Ukinion@users.noreply.github.com>
2023-02-04 11:55:40 +0700

    Changed code style

--- T4/src/turple_printer.h
+++ T4/src/turple_printer.h
@@ -4,31 +4,28 @@
 #include <tuple>
 #include <fstream>
 
-template <typename Ch, typename Tr, size_t last_pos_in_tuple, typename... Args>
-struct Printer {
-  static std::basic_ostream<Ch, Tr>& tuple_print(
-      std::basic_ostream<Ch, Tr>& os, std::tuple<Args...> const& tuple) {
-    Printer<Ch, Tr, last_pos_in_tuple - 1, Args...>::tuple_print(os, tuple);
-    return os << std::get<last_pos_in_tuple - 1>(tuple) << '|';
-  }
+template <typename Ch, typename Tr, size_t pos_int_tuple, typename... Args>
+class Printer
+{
+public:
+    static std::basic_ostream<Ch, Tr>& PrintTuple(std::basic_ostream<Ch, Tr>& os, std::tuple<Args...> const& tuple)
+    {
+        Printer<Ch, Tr, pos_int_tuple - 1, Args...>::PrintTuple(os, tuple);
+        return os << std::get<pos_int_tuple - 1>(tuple) << ' ';
+    }
 };
 
 template <typename Ch, typename Tr, typename... Args>
-struct Printer<Ch, Tr, 1, Args...> {
-  static std::basic_ostream<Ch, Tr>& tuple_print(
-      std::basic_ostream<Ch, Tr>& os, std::tuple<Args...> const& tuple) {
-    return os << std::get<0>(tuple) << '|';
-  }
+class Printer<Ch, Tr, 1, Args...>
+{
+public:
+    static std::basic_ostream<Ch, Tr>& PrintTuple (std::basic_ostream<Ch, Tr>& os, std::tuple<Args...> const& tuple)
+    { return os << std::get<0>(tuple) << ' '; }
 };
 
 template <typename Ch, typename Tr>
-std::basic_ostream<Ch, Tr>& operator<<(std::basic_ostream<Ch, Tr>& os,
-                                       std::tuple<> const& t) {
-  return os;
-}
+std::basic_ostream<Ch, Tr>& operator<<(std::basic_ostream<Ch, Tr>& os, std::tuple<> const& t) { return os; }
 
 template <typename Ch, typename Tr, typename... Args>
-std::basic_ostream<Ch, Tr>& operator<<(std::basic_ostream<Ch, Tr>& os,
-                                       std::tuple<Args...> const& tuple) {
-  return Printer<Ch, Tr, sizeof...(Args), Args...>::tuple_print(os, tuple);
-}
+std::basic_ostream<Ch, Tr>& operator<<(std::basic_ostream<Ch, Tr>& os, std::tuple<Args...> const& tuple)
+{ return Printer<Ch, Tr, sizeof...(Args), Args...>::PrintTuple(os, tuple); }
